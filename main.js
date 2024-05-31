d2407cb288d47f49be746aa62035c3f690d5357d
Martin Heide <martin@heide>
2024-05-31 23:19:24 +0200

    Add brotli-wasm

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ import store from './store.js'
 
 // import { init, compress, decompress } from '@bokuweb/zstd-wasm';
 import { zlibSync, unzlibSync, gzipSync, gunzipSync, deflateSync, inflateSync } from 'fflate';
+import brotliPromise from 'brotli-wasm'; // Import the default export
+
 
 export const vue = createApp({
   template: /*html*/`<router-view />`
@@ -31,7 +33,25 @@ if (document.getElementById('app')) {
 
 
 (async () => {
-  const toCompressStr = `import { defineAsyncComponent } from 'vue'
+  const toCompressStr = `import { createApp } from 'vue'
+import router from './router.js'
+import store from './store.js'
+
+// import { init, compress, decompress } from '@bokuweb/zstd-wasm';
+import { zlibSync, unzlibSync, gzipSync, gunzipSync, deflateSync, inflateSync } from 'fflate';
+
+export const vue = createApp({
+  template: /*html*/'<router-view />'
+})
+
+vue.use(store)
+vue.use(router)
+
+if (document.getElementById('app')) {
+  vue.mount('#app')
+}
+
+import { defineAsyncComponent } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const App = defineAsyncComponent(() => import('./component-app.js'))
@@ -48,6 +68,7 @@ const router = createRouter({
 
 export default router`;
   // console.log('toCompressStr', toCompressStr.length);
+
   const toCompress = new TextEncoder().encode(toCompressStr);
   console.log('toCompress', toCompress.byteLength, toCompress);
   let compressed = zlibSync(toCompress, { level: 9 });
@@ -73,4 +94,22 @@ export default router`;
   decompressedStr = new TextDecoder().decode(decompressed);
   // console.log('decompressedStr', decompressedStr);
   console.assert(decompressedStr === toCompressStr, 'DEFLATE not equal');
+
+  // Decompresses to string instead of Uint8Array?!
+  // compressed = new Uint8Array(LZMA.compress(toCompress, 9));
+  // console.log('compressed LZMA', compressed.byteLength, compressed); // 188 bytes
+  // decompressed = LZMA.decompress(compressed);
+  // console.log('decompressed', decompressed);
+  // decompressedStr = new TextDecoder().decode(decompressed);
+  // // console.log('decompressedStr', decompressedStr);
+  // console.assert(decompressedStr === toCompressStr, 'LZMA not equal');
+
+  const brotli = await brotliPromise; // Import is async in browsers due to wasm requirements!
+  compressed = brotli.compress(toCompress, { level: 9 });
+  console.log('compressed BROTLI', compressed.byteLength, compressed); // 188 bytes
+  decompressed = brotli.decompress(compressed);
+  console.log('decompressed', decompressed);
+  decompressedStr = new TextDecoder().decode(decompressed);
+  // console.log('decompressedStr', decompressedStr);
+  console.assert(decompressedStr === toCompressStr, 'BROTLI not equal');
 })();
