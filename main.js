41333180219af9a2fd4ba5361ab097d0c15e58d9
Martin Heide <martin.heide@gmx.net>
2024-06-01 00:12:38 +0200

    Results

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ import store from './store.js'
 // import { init, compress, decompress } from '@bokuweb/zstd-wasm';
 import { zlibSync, unzlibSync, gzipSync, gunzipSync, deflateSync, inflateSync } from 'fflate';
 import brotliPromise from 'brotli-wasm'; // Import the default export
+const brotli = await brotliPromise; // Import is async in browsers due to wasm requirements!
 
+// const brotli = await import("https://unpkg.com/brotli-wasm@3.0.0/index.web.js?module").then(m => m.default);
 
 export const vue = createApp({
   template: /*html*/`<router-view />`
@@ -72,7 +74,7 @@ export default router`;
   const toCompress = new TextEncoder().encode(toCompressStr);
   console.log('toCompress', toCompress.byteLength, toCompress);
   let compressed = zlibSync(toCompress, { level: 9 });
-  console.log('compressed ZLIB', compressed.byteLength, compressed); // 194 bytes
+  console.log('compressed ZLIB', compressed.byteLength, compressed);
   let decompressed = unzlibSync(compressed);
   console.log('decompressed', decompressed);
   let decompressedStr = new TextDecoder().decode(decompressed);
@@ -80,7 +82,7 @@ export default router`;
   console.assert(decompressedStr === toCompressStr, 'ZLIB not equal');
 
   compressed = gzipSync(toCompress, { level: 9 });
-  console.log('compressed GZIP', compressed.byteLength, compressed); // 206 bytes
+  console.log('compressed GZIP', compressed.byteLength, compressed);
   decompressed = gunzipSync(compressed);
   console.log('decompressed', decompressed);
   decompressedStr = new TextDecoder().decode(decompressed);
@@ -88,7 +90,7 @@ export default router`;
   console.assert(decompressedStr === toCompressStr, 'GZIP not equal');
 
   compressed = deflateSync(toCompress, { level: 9 });
-  console.log('compressed DEFLATE', compressed.byteLength, compressed); // 188 bytes
+  console.log('compressed DEFLATE', compressed.byteLength, compressed);
   decompressed = inflateSync(compressed);
   console.log('decompressed', decompressed);
   decompressedStr = new TextDecoder().decode(decompressed);
@@ -97,19 +99,24 @@ export default router`;
 
   // Decompresses to string instead of Uint8Array?!
   // compressed = new Uint8Array(LZMA.compress(toCompress, 9));
-  // console.log('compressed LZMA', compressed.byteLength, compressed); // 188 bytes
+  // console.log('compressed LZMA', compressed.byteLength, compressed);
   // decompressed = LZMA.decompress(compressed);
   // console.log('decompressed', decompressed);
   // decompressedStr = new TextDecoder().decode(decompressed);
   // // console.log('decompressedStr', decompressedStr);
   // console.assert(decompressedStr === toCompressStr, 'LZMA not equal');
 
-  const brotli = await brotliPromise; // Import is async in browsers due to wasm requirements!
-  compressed = brotli.compress(toCompress, { level: 9 });
-  console.log('compressed BROTLI', compressed.byteLength, compressed); // 188 bytes
+  compressed = brotli.compress(toCompress, { quality: 11 });
+  console.log('compressed BROTLI', compressed.byteLength, compressed);
   decompressed = brotli.decompress(compressed);
   console.log('decompressed', decompressed);
   decompressedStr = new TextDecoder().decode(decompressed);
   // console.log('decompressedStr', decompressedStr);
   console.assert(decompressedStr === toCompressStr, 'BROTLI not equal');
+
+  // Results:
+  // GZIP    401
+  // ZLIB    389
+  // DEFLATE 383
+  // BROTLI  348 bytes
 })();
